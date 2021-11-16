99e8a4edea3ad2c5362b3adbb3cea59edce32cf3
Gleb Pashchenko <pashenkogleb@gmail.com>
2021-11-17 00:37:31 +0300

    created tree structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='graph_profile',
+    url='https://github.com/pashenkogleb/graph_profile',
+    author='Gleb Pashchenko',
+    author_email='pashenkogleb@gmail.com',
+    # Needed to actually package something
+    packages=['graph_profile'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='creates call graph ',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.md').read(),
+)
