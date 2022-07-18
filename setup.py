ce41b20aa41be363faf83f83e8d461f646f900c4
Gleb Pashchenko <pashenkogleb@gmail.com>
2022-07-18 17:37:29 +0400

    fixed dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=find_packages(),
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=[],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
