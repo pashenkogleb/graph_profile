5d2cf7191c8cb1fbf2c642658fb678655a7da584
Gleb Pashchenko <pashenkogleb@gmail.com>
2022-07-18 17:28:54 +0400

    finalized

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # graph_profile
 
 IPython magic to create call graph with timings
+
+
+# usage
+
+to install:
+
+```
+pip install git+https://github.com/pashenkogleb/graph_profile
+```
+
+to use:
+```
+import graph_profile
+
+%%graph_profile [optional_filename for jpg image]
+CODE
+```
+
+
+# how it works
+
+cprofile to create profiler objects
+
+gprof2dot to create dot graph
+
+```
+%%prun -q -D out.profile
+SOME_CODE
+```
+```
+! gprof2dot -f pstats out.profile  > temp.dot
+! dot -Tjpg temp.dot -o outfile.jpg
+```
--- graph_profile/src.py
+++ graph_profile/src.py
@@ -1,12 +1,40 @@
 import IPython
 from IPython.core.magic import register_cell_magic
-
+import tempfile
 
 
 @register_cell_magic
 def graph_profile(line, cell):
+    '''
+    can provide file name in line 
+    '''
+
+    if line =='':
+        jpg_file = tempfile.NamedTemporaryFile(suffix = ".jpg")
+    else:
+        jpg_file = open(line, "w")
+    
     ipython = IPython.get_ipython()
-    ipython.run_cell_magic("prun", "-D /tmp/temprorary_profile.profile", cell)
-    ipython.system("gprof2dot -f pstats /tmp/temprorary_profile.profile  > /tmp/temprorary_profile.dot")
-    ipython.system("dot -Tjpg /tmp/temprorary_profile.dot -o profile_res.jpg")
-    print("output in profile_res.jpg")
+    profile_file = tempfile.NamedTemporaryFile(suffix = ".profile")
+    dot_file = tempfile.NamedTemporaryFile(suffix = ".dot")
+    
+
+    ipython.run_cell_magic("prun", f"-q -D {profile_file.name}", cell) #q to supress output
+    ipython.system(f"gprof2dot -f pstats {profile_file.name}  > {dot_file.name}")
+    ipython.system(f"dot -Tjpg {dot_file.name} -o {jpg_file.name}")
+    
+
+    IPython.display.Image(filename = jpg_file.name)
+
+    res =  IPython.display.Image(filename = f"{jpg_file.name}", format= "jpg")
+    for f in [profile_file,dot_file,jpg_file]:
+        f.close()
+
+
+    return res
+
+    # ipython.run_cell_magic("prun", "-D /tmp/temprorary_profile.profile", cell)
+    #ipython.system("gprof2dot -f pstats /tmp/temprorary_profile.profile  > /tmp/temprorary_profile.dot")
+
+    #ipython.system("dot -Tjpg /tmp/temprorary_profile.dot -o profile_res.jpg")
+    #print("output in profile_res.jpg")
